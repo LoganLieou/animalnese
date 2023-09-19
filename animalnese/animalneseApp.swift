7393b24a390e98fe4d99381ffc569da0af56111d
LoganLieou <logan.lieou@gmail.com>
2023-09-18 23:29:43 -0500

    Initial Commit

---
+++ animalnese/animalneseApp.swift
@@ -0,0 +1,17 @@
+//
+//  animalneseApp.swift
+//  animalnese
+//
+//  Created by Logan  Jackson on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct animalneseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
