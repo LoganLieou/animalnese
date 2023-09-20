179377c89e6b5c1c36f06f651d0e2e0ce78459e8
LoganLieou <logan.lieou@gmail.com>
2023-09-19 19:10:26 -0500

    first

--- animalnese/ContentView.swift
+++ animalnese/ContentView.swift
@@ -6,14 +6,48 @@
 //
 
 import SwiftUI
+import AVFoundation
+import CoreMIDI
+
+let synth = AVSpeechSynthesizer()
+
+func clean(_ text: String) -> String {
+    var result = ""
+    for c in text {
+        if c.isLetter {
+            result += c.lowercased()
+        }
+    }
+    return result
+}
+
+func playSound(_ text: String) {
+    // need to figure how it's generated...
+    let cleanText = clean(text)
+    for c in cleanText {
+        let speechUtterence = AVSpeechUtterance(string: "\(c)")
+        speechUtterence.rate = MAXFLOAT
+        speechUtterence.pitchMultiplier = Float.random(in: -2.0..<0.0)
+        speechUtterence.voice = AVSpeechSynthesisVoice(language: "en-US")
+        synth.speak(speechUtterence)
+    }
+}
 
 struct ContentView: View {
+    @State private var text: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Animalnese")
+                .font(.title)
+            TextField("Enter words...", text: $text)
+                .multilineTextAlignment(.center)
+            Button(action: {
+                playSound(text)
+            }) {
+                Text("Speak!")
+            }
+            .buttonStyle(.bordered)
+            .padding()
         }
         .padding()
     }
